fa5a51069e10d789e6ad5e8c314b72cd8704d106
TehBigMick <134397911+TehBigMick@users.noreply.github.com>
2026-04-30 10:29:23 +0800

    Update phonics-data.js

--- phonics-data.js
+++ phonics-data.js
@@ -1,6 +1,6 @@
 const PHONICS_DATA = {
   level1: {
     phonemes: ["s","a","t","p","i","n","m","d"],
-    words: ["sat","sit","tap","pin","pan","mat","dip"]
+    words: ["sat","sit","tap","pin","pan","mat","dip","man","pit","sad"]
   }
 };
