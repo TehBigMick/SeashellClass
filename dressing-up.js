562fe299cb203eb6fc416ed4cd905f9dfeca8185
TehBigMick <134397911+TehBigMick@users.noreply.github.com>
2026-05-21 15:54:47 +0800

    Update dressing-up.js

--- dressing-up.js
+++ dressing-up.js
@@ -1,1 +1,14 @@
+const dressingPages = [
+  { image: 'dressing-up1.webp', teacherText: '', teacherAudio: '', cvcWords: [] }, // Title
+  { image: 'dressing-up2.webp', teacherText: "I'd like to be a mermaid.", teacherAudio: 'dressing1.m4a', cvcWords: ['i', 'can', 'sit', 'on', 'a', 'rock'] },
+  { image: 'dressing-up3.webp', teacherText: "Or maybe a circus acrobat -", teacherAudio: 'dressing2.m4a', cvcWords: ['i', 'can', 'get', 'up', 'on', 'top'] },
+  { image: 'dressing-up4.webp', teacherText: "Or how about a grizzly bear?", teacherAudio: 'dressing3.m4a', cvcWords: ['i', 'can', 'dig', 'a', 'den'] },
+  { image: 'dressing-up5.webp', teacherText: "I'd like to sail the seven seas.", teacherAudio: 'dressing4.m4a', cvcWords: ['i', 'can', 'mop', 'a', 'deck'] },
+  { image: 'dressing-up6.webp', teacherText: "Or watch me score the winning goal -", teacherAudio: 'dressing5.m4a', cvcWords: ['i', 'can', 'kick', 'at', 'a', 'net'] },
+  { image: 'dressing-up7.webp', teacherText: "I could be an explorer.", teacherAudio: 'dressing6.m4a', cvcWords: ['i', 'can', 'pack', 'a', 'map'] },
+  { image: 'dressing-up8.webp', teacherText: "Or maybe a sleeping princess.\nHush now!", teacherAudio: 'dressing7.m4a', cvcWords: ['i', 'can', 'nap'] },
+  { image: 'dressing-up9.webp', teacherText: "Perhaps I'll be a superstar chef?", teacherAudio: 'dressing8.m4a', cvcWords: ['i', 'can', 'pick', 'up', 'a', 'pan'] },
+  { image: 'dressing-up10.webp', teacherText: "I can be anything I like,", teacherAudio: 'dressing9.m4a', cvcWords: ['i', 'can', 'i', 'can', 'i', 'can'] }
+];
 
+let currentDressingPage = 0;
